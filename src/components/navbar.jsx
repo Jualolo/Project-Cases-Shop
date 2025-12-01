58186218a4319a11d012bc78e3028a5fd9b26da9
Juan Lozano <jalozanolopez@gmail.com>
2025-12-01 19:30:40 +0100

    Edit navbar, add css

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './navbar.css';
+
+// Optional: in the future you can import an image like this:
+// import logo from '../assets/logo.png';
+// and uncomment the <img> tag inside the brand div below.
 
 export const Navbar = () => {
   return (
     <div className="navbar">
+      <div className="nav-inner">
+        <div className="brand">
+          {/* placeholder brand text (replace with an <img src={logo} alt="Logo" /> when available) */}
+          <span>Character Cases</span>
+          {/* Example image using an imported asset: */}
+          {/* <img src={logo} alt="MyShop logo" /> */}
+        </div>
         <div className="links">
           <Link to="/">Shop</Link>
           <Link to="/cart">Cart</Link>
         </div>
+      </div>
     </div>
   );
 }
