fcd0c6b0cfd8bba7932d1b7cfc070d55711af628
Juan Lozano <jalozanolopez@gmail.com>
2025-12-03 08:37:07 +0100

    Add quantity buttons cart

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
 
 export const CartItem = (props) => {
 
     const {id, productName, price} = props.data;
+    const {cartItems, addToCart, removeFromCart, updateCartItemCount} = React.useContext(ShopContext);
+    const cartItemAmount = cartItems[id];
 
     return (
         <div className="cartItem">
@@ -10,7 +14,14 @@ export const CartItem = (props) => {
             <div className="cartItemDetails">
                 <h3>{props.data.productName}</h3>
                 <p>Price: {props.data.price}€</p>
-                <p>Quantity: {/* Quantity logic to be added */}</p>
+                
+                <div className="countHandler">
+                    <button onClick={() => removeFromCart(id)}>-</button>
+                    <input value={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id)}/>
+                    <button onClick={() => addToCart(id)}>+</button>
+
+                </div>
+
             </div>
         </div>
     )
